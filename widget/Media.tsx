262d2b86ddd8586cbaf7e3773fe62db983dc3fc4
Brendan Early <mymindstorm@evermiss.net>
2026-01-18 16:33:35 -0600

    basic media

--- widget/Media.tsx
+++ widget/Media.tsx
@@ -1,17 +1,46 @@
 import AstalMpris from "gi://AstalMpris"
-import { createBinding, For } from "gnim"
+import { createBinding, createState, For, With } from "gnim"
+
+const PAUSE_ICON = ""
+const PLAY_ICON = ""
 
 export default function Media() {
   const mpris = AstalMpris.get_default()
-  const players = createBinding(mpris, "players")
+  const [title, setTitle] = createState("")
+
+  mpris.connect("player-added", (_, player) => {
+    player.connect("notify", (player) => {
+      if (player.playbackStatus === AstalMpris.PlaybackStatus.STOPPED) {
+        setTitle("")
+        return
+      }
+
+      const icon =
+        player.playbackStatus === AstalMpris.PlaybackStatus.PLAYING
+          ? PLAY_ICON
+          : PAUSE_ICON
+
+      setTitle(`${icon} ${player.title}`)
+    })
+  })
+
+  mpris.connect("player-closed", (_, player) => {
+    if (title() === player.title) {
+      setTitle("")
+    }
+  })
 
   return (
-    <menubutton>
-      <For each={players}>
-        {(player) => {
-          return <label label={`${player.title} by ${player.artist}. `} />
-        }}
-      </For>
-    </menubutton>
+    <box>
+      <With value={title}>
+        {(title) =>
+          title && (
+            <menubutton>
+              <label label={title} />
+            </menubutton>
+          )
+        }
+      </With>
+    </box>
   )
 }
