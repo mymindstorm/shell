3c010a7528f97d0e3b176b4c8701f4502832257d
Brendan Early <mymindstorm@evermiss.net>
2026-01-19 02:42:01 -0600

    Bluetooth done
    Thank you AI, I didn't want to write all that tedious UI

--- widget/Bluetooth.tsx
+++ widget/Bluetooth.tsx
@@ -6,19 +6,27 @@ export default function Bluetooth() {
   const bt = AstalBluetooth.get_default()
   const btOn = createBinding(bt, "isPowered")
   const devices = createBinding(bt, "devices")
-  const [numConnected, setNumConnected] = createState(0)
 
+  const [numConnected, setNumConnected] = createState(0)
   const updateConnected = () =>
     setNumConnected(devices().filter((d) => d.connected).length)
   updateConnected()
 
   const watchDevice = (device: AstalBluetooth.Device) =>
-    device.connect("notify::connected", () => updateConnected())
+    device.connect("notify:: connected", () => updateConnected())
 
   bt.connect("device-removed", (_, device) => watchDevice(device))
   bt.connect("device-added", (_, device) => watchDevice(device))
 
   bt.devices.forEach(watchDevice)
+
+  const toggleConnection = (device: AstalBluetooth.Device) => {
+    if (device.connected) {
+      device.disconnect_device(() => {})
+    } else {
+      device.connect_device(() => {})
+    }
+  }
 
   return (
     <menubutton>
@@ -31,31 +39,119 @@ export default function Bluetooth() {
         <With value={btOn}>
           {(btOn) => {
             const icon = btOn ? "bluetooth-symbolic" : "bluetooth-none-symbolic"
-
             return <image iconName={icon} />
           }}
         </With>
       </box>
       <popover>
-        <box spacing={4} orientation={Gtk.Orientation.VERTICAL}>
+        <box
+          orientation={Gtk.Orientation.VERTICAL}
+          cssClasses={["bluetooth-popover"]}
+          spacing={4}
+        >
+          {/* Device List */}
           <For each={devices}>
             {(device) => {
               const connected = createBinding(device, "connected")
-              const connStr = createComputed(() =>
-                connected() ? "Connected" : "",
+              const connecting = createBinding(device, "connecting")
+              const icon = createBinding(device, "icon")
+              const name = createBinding(device, "name")
+
+              const statusLabel = createComputed(() => {
+                if (connecting()) return "Connecting..."
+                if (connected()) return "Connected"
+                return "Disconnected"
+              })
+
+              const buttonLabel = createComputed(() => {
+                if (connecting()) return "..."
+                return connected() ? "Disconnect" : "Connect"
+              })
+
+              const statusClass = createComputed(() =>
+                connected() ? "status-connected" : "status-disconnected",
               )
 
               return (
                 <box
+                  cssClasses={["device-row"]}
+                  spacing={12}
                   valign={Gtk.Align.CENTER}
-                  orientation={Gtk.Orientation.VERTICAL}
                 >
-                  <label xalign={0} label={connStr} />
-                  <label xalign={0} label={createBinding(device, "name")} />
+                  {/* Device Icon */}
+                  <With value={icon}>
+                    {(iconName) => (
+                      <image
+                        iconName={iconName || "bluetooth-symbolic"}
+                        pixelSize={24}
+                        cssClasses={["device-icon"]}
+                      />
+                    )}
+                  </With>
+
+                  {/* Device Info */}
+                  <box
+                    orientation={Gtk.Orientation.VERTICAL}
+                    hexpand
+                    valign={Gtk.Align.CENTER}
+                    spacing={2}
+                  >
+                    <label
+                      xalign={0}
+                      label={name}
+                      cssClasses={["device-name"]}
+                      ellipsize={3}
+                      maxWidthChars={20}
+                    />
+                    <With value={statusClass}>
+                      {(statusCls) => (
+                        <label
+                          xalign={0}
+                          label={statusLabel}
+                          cssClasses={["device-status", statusCls]}
+                        />
+                      )}
+                    </With>
+                  </box>
+
+                  {/* Connect/Disconnect Button */}
+                  <With value={connecting}>
+                    {(isConnecting) => (
+                      <button
+                        cssClasses={["device-button"]}
+                        sensitive={!isConnecting}
+                        onClicked={() => toggleConnection(device)}
+                        valign={Gtk.Align.CENTER}
+                      >
+                        <label label={buttonLabel} />
+                      </button>
+                    )}
+                  </With>
                 </box>
               )
             }}
           </For>
+
+          {/* Empty State */}
+          <With value={devices}>
+            {(deviceList) =>
+              deviceList.length === 0 ? (
+                <box
+                  cssClasses={["empty-state"]}
+                  orientation={Gtk.Orientation.VERTICAL}
+                  spacing={8}
+                  valign={Gtk.Align.CENTER}
+                  halign={Gtk.Align.CENTER}
+                >
+                  <image
+                    iconName="bluetooth-disabled-symbolic"
+                    pixelSize={48}
+                  />
+                  <label label="No devices found" cssClasses={["dim-label"]} />
+                </box>
+              ) : null
+            }
+          </With>
         </box>
       </popover>
     </menubutton>
